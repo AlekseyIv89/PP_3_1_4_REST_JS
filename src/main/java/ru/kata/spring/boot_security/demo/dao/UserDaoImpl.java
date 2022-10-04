250d7192cf5af104714ede58ec48bd704ee42456
Aleksey <alekseyiv89@mail.ru>
2022-10-04 21:43:13 +0300

    Spring Security

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
@@ -1,14 +1,11 @@
 package ru.kata.spring.boot_security.demo.dao;
 
 import org.springframework.stereotype.Repository;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Repository
 public class UserDaoImpl implements UserDao {
